0b0d137e052ab843102361441c0392dd2bfb8bbf
M irsyad Ibrahim faqih <144421036+mirsyadibrahimfaqih@users.noreply.github.com>
2023-10-12 21:30:20 +0700

    Update script.js

--- index.js/script.js
+++ index.js/script.js
@@ -1,15 +1,19 @@
+let nama = prompt("masukan kata 'Merah Putih' untuk mengubah ke toUpperCase")
 let teks = "Merah Putih"
 let uppercase = teks.toUpperCase()
 alert(uppercase)
 
+let nama1 = prompt("masukan kata 'Merah Putih' untuk mengubah ke toLowerCase")
 let teks2 = "Merah Putih"
 let lowercase = teks2.toLowerCase()
 alert(lowercase)
 
+let ubah = prompt("masukan kata 'Putih' untuk mengubah kata putih ke muda")
 let teks3 = "Merah Putih"
 let ubahteks = teks3.replace("Putih","Muda")
 alert(ubahteks)
 
+let panjang = prompt("masukan kata 'Merah Putih' untuk mengukur panjang")
 let teks4 = "Merah Putih"
 let cek = teks4.length
 alert(cek)
