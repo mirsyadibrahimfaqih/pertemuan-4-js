aec4a423b34e181ab777e678dd6487567573db92
M irsyad Ibrahim faqih <144421036+mirsyadibrahimfaqih@users.noreply.github.com>
2023-10-12 13:42:46 +0700

    Add files via upload

---
+++ index.js/script.js
@@ -0,0 +1,18 @@
+let teks = "Merah Putih"
+let uppercase = teks.toUpperCase()
+alert(uppercase)
+
+let teks2 = "Merah Putih"
+let lowercase = teks2.toLowerCase()
+alert(lowercase)
+
+let teks3 = "Merah Putih"
+let ubahteks = teks3.replace("Putih","Muda")
+alert(ubahteks)
+
+let teks4 = "Merah Putih"
+let cek = teks4.length
+alert(cek)
+
+
+
